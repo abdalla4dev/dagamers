e912e1b03ef2d834370831038991ab90baf1d49f
xariaa <xariaa@gmail.com>
2011-02-28 18:13:48 +0000

    added DFS and BFS search for tree.

--- Assets/Scripts/TreeNode.cs
+++ Assets/Scripts/TreeNode.cs
@@ -58,13 +58,58 @@ public class TreeNode {
 	}
 	
 	public void printMania() {
-		for (int i=0;i<4;i++) {
+		/*for (int i=0;i<1;i++) {
 			QnNode curr = new QnNode();
 			curr = (QnNode)startNode[i];
 			Debug.Log(curr.getQn());
 			Debug.Log(curr.getAnswer());
 			Debug.Log(curr.getPerson());
 			Debug.Log(curr.getNumOfNextNodes());
+			if(curr.getNumOfNextNodes()>0)
+				Debug.Log(curr.getNumOfNextNodes());
+		}*/
+		//BFS((QnNode)startNode[0]);
+		DFS((QnNode)startNode[0]);
+	}
+	
+	public void BFS(QnNode start) {
+		Queue questions = new Queue();
+		QnNode curr = new QnNode();
+		
+		questions.Enqueue(start);
+		while (questions.Count>0)
+		{
+			curr = (QnNode) questions.Dequeue();
+			if(curr.getNumOfNextNodes()>0)
+			{
+				ArrayList next = new ArrayList();
+				next = curr.getNextNodeList();
+				for(int i=0; i<next.Count; i++)
+				{
+					questions.Enqueue((QnNode)next[i]);
+				}
+			}
+			printIt(curr);
 		}
+			
+	}
+	
+	public void DFS(QnNode start)
+	{
+		printIt(start);
+		if(start.getNumOfNextNodes()>0)
+		{
+			ArrayList next = new ArrayList();
+			next = start.getNextNodeList();
+			for(int i=0; i<next.Count; i++)
+			{
+				DFS((QnNode)next[i]);
+			}
+		}
+			return;
+	}
+	
+	public void printIt(QnNode node) {
+		Debug.Log(node.getQn() + "qn");
 	}
 }
