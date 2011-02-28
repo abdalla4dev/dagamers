4de1cae810b84aac37a0b88b7c9c85127ef41b26
jxylow@gmail.com <jxylow@gmail.com@ab7b2411-665f-f69e-c7aa-cc4f6e78dff1>
2011-02-28 07:24:15 +0000

    Completed MouseOverScript.cs

--- trunk/Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseOverScript.cs
+++ trunk/Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseOverScript.cs
@@ -1,36 +1,32 @@
 using UnityEngine;
 using System.Collections;
 
+
 public class MouseOverScript : MonoBehaviour {
 	
+	public Transform targetObject;
 	Color mouseOverColor = Color.yellow;
 	bool isSelected = false;
 	bool withinBoundary = false;
-	//array to store the original material color
-	Color[] originalColor;
+	bool actionKey = false;
+	Color[] originalColor; //array to store the original material color
+	Vector3 offset = Vector3.up;
+	Vector3 screenPos;
 	
 	// Use this for initialization
 	void Start () {
-		originalColor = new Color[renderer.materials.Length];
-		for(int i = 0; i < renderer.materials.Length; i++){
-			originalColor[i] = renderer.materials[i].color;
+		originalColor = new Color[targetObject.renderer.materials.Length];
+		for(int i = 0; i < targetObject.renderer.materials.Length; i++){
+			originalColor[i] = targetObject.renderer.materials[i].color;
 		}
 	}
 	
 	void OnMouseEnter(){
-		//changing all the material to be yellow upon mouse hover
-		for(int i = 0; i < renderer.materials.Length; i++){
-			renderer.materials[i].color = mouseOverColor;
-		}
-	}
-	
-	void OnMouseDown(){
-		//to toggle the selection mode
-		if(isSelected){
-				isSelected = false;
-		}
-		else{
-			isSelected = true;
+		//changing all the material to be yellow upon mouse hover, once the player is close to the object
+		if(withinBoundary){
+			for(int i = 0; i < targetObject.renderer.materials.Length; i++){
+				targetObject.renderer.materials[i].color = mouseOverColor;
+			}
 		}
 	}
 	
@@ -38,23 +34,23 @@ public class MouseOverScript : MonoBehaviour {
 		//if user did not select the object
 		if(isSelected == false){
 			//changes back to the original material upon mouse exit
-			for(int i = 0; i < renderer.materials.Length; i++){
-				renderer.materials[i].color = originalColor[i];
+			for(int i = 0; i < targetObject.renderer.materials.Length; i++){
+				targetObject.renderer.materials[i].color = originalColor[i];
 			}
 		}
 	}
 	
-	void OnCollisionEnter(Collision collision){
+	void OnTriggerEnter(Collider other){
 		withinBoundary = true;
 	}
 	
-	void OnCollisionExit(Collision collision){
+	void OnTriggerExit(Collider other){
 		withinBoundary = false;
 	}
 	
 	void OnGUI() {
-		if(isSelected == true && withinBoundary == true){
-			GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "This is a title");
+		if(isSelected == true && withinBoundary == true && actionKey == true){
+			GUI.Box(new Rect(screenPos.x, (Screen.height - screenPos.y), 100, 100), "Hello World!");
 		}
 	}
 		
@@ -62,5 +58,25 @@ public class MouseOverScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		screenPos = Camera.main.WorldToScreenPoint(targetObject.position + offset);
+		
+		if(Input.GetMouseButtonDown(0)){
+			
+			if(withinBoundary == true){
+				actionKey = !actionKey;
+				
+				if(actionKey){
+					isSelected = true;
+				}
+				else{
+					isSelected = false;
+				}
+			}
+		}
+		
+		if(!(isSelected == true && withinBoundary == true)){
+			actionKey = false;
+			isSelected = false;
+		}
 	}
 }
