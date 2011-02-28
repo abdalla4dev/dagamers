62842959c018d794c7bd0adccc4fdd7b6f94740f
wirelessed <wirelessed@ab7b2411-665f-f69e-c7aa-cc4f6e78dff1>
2011-02-28 08:36:50 +0000

    added prefabs and colliders

--- Assets/Scripts/AddColliders.cs
+++ Assets/Scripts/AddColliders.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class AddColliders : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
