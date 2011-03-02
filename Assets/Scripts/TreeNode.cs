f708170c517e442cad93a3ccef9f591f82b74734
xariaa <xariaa@gmail.com>
2011-03-02 03:51:44 +0000

    readded checker.

--- Assets/Scripts/TreeNode.cs
+++ Assets/Scripts/TreeNode.cs
@@ -81,14 +81,83 @@ public class TreeNode : MonoBehaviour {
 		currNode.Add(temp);
 	}
 	
-	public void printMania() {
-		for (int i=0;i<4;i++) {
+	public void checker() {
+		/*for (int i=0;i<1;i++) {
 			QnNode curr = new QnNode();
 			curr = (QnNode)startNode[i];
 			Debug.Log(curr.getQn());
 			Debug.Log(curr.getAnswer());
 			Debug.Log(curr.getPerson());
 			Debug.Log(curr.getNumOfNextNodes());
+			if(curr.getNumOfNextNodes()>0)
+				Debug.Log(curr.getNumOfNextNodes());
+		}*/
+		
+		int unlockedCounter=0;
+		for(int i=0; i<4; i++)
+		{
+			QnNode curr = new QnNode();
+			curr = (QnNode)startNode[i];
+		//unlockedCounter += BFS((QnNode)startNode[0]);
+		unlockedCounter += DFS((QnNode)startNode[i]);
 		}
+		
+		float currProgress = checkProgress(unlockedCounter);
+	}
+	
+	float playTime, totalTime;
+	float totalCounter;
+	float checkProgress(int unlockedCounter) {
+		float qnProgress = (unlockedCounter/totalCounter); 
+		float timeProgress = (playTime/totalTime);
+		float totalProgress = (qnProgress/timeProgress)*100;
+		return totalProgress;
+	}
+	
+	public int BFS(QnNode start) {
+		int unlocked=0;
+		Queue questions = new Queue();
+		QnNode curr = new QnNode();
+		
+		questions.Enqueue(start);
+		while (questions.Count>0)
+		{
+			curr = (QnNode) questions.Dequeue();
+			if(curr.getNumOfNextNodes()>0)
+			{
+				ArrayList next = new ArrayList();
+				next = curr.getNextNodeList();
+				for(int i=0; i<next.Count; i++)
+				{
+					questions.Enqueue((QnNode)next[i]);
+				}
+			}
+			printIt(curr);
+			if(curr.getUnlockedNode())
+				unlocked++;
+		}
+		return unlocked;		
+	}
+	
+	public int DFS(QnNode start)
+	{
+		int unlocked=0;
+		printIt(start);
+		if(start.getUnlockedNode())
+			unlocked++;
+		if(start.getNumOfNextNodes()>0)
+		{
+			ArrayList next = new ArrayList();
+			next = start.getNextNodeList();
+			for(int i=0; i<next.Count; i++)
+			{
+				DFS((QnNode)next[i]);
+			}
+		}
+			return unlocked;
+	}
+	
+	public void printIt(QnNode node) {
+		Debug.Log(node.getPerson() + " " + node.getQn() + " " + node.getAnswer());
 	}
 }
