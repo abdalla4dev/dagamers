fe82340537bb434a5f30734853c132d818a6ba26
menggnem <menggnem@ab7b2411-665f-f69e-c7aa-cc4f6e78dff1>
2011-03-05 04:15:34 +0000

    All activities discussed yesterday is in MurderData.cs How to access data is in AccessingMuderData.cs

---
+++ Assets/Scripts/GenerateTimeline.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System;
+using MurderData;
+
+public class GenerateTimeline 
+{
+	//implement the CSP solver here
+	/*
+	 * 1) Generate a muderer x from Suspects
+	 * 2) Access the data and give x a weapon related activity one hour before murder
+	 * 3) Give x a false activity to cover up for (2)
+	 * 4) Give a weapon related activity to another suspect. 
+	 * 5) Give an alibi for (4)
+	 * 6) Fill the rest of the timeline with Generic_Activities
+	 * 
+	 */
+}
