e3bd831cf4d9080687b17ca0590f9c6227a4a1c5
menggnem <menggnem@ab7b2411-665f-f69e-c7aa-cc4f6e78dff1>
2011-03-05 04:15:34 +0000

    All activities discussed yesterday is in MurderData.cs How to access data is in AccessingMuderData.cs

--- trunk/Assets/Scripts/AccessingMurderData.cs
+++ trunk/Assets/Scripts/AccessingMurderData.cs
@@ -5,19 +5,28 @@ using MurderData;
 
 public class AccessingMurderData : MonoBehaviour {
 	
-	private readonly Kitchen theKitchen = new Kitchen();
-	
 	// Use this for initialization
 	void Start () {
-		Debug.Log(theKitchen.knife.ka);
-		foreach (string s in Enum.GetNames(typeof(Knife.Knife_Activities))) {
-			Debug.Log(s + " " + (int)Enum.Parse(typeof(Knife.Knife_Activities), s));
+		Debug.Log(Kitchen.knife.activity); //access the only possible activity of the knife in the kitchen
+		foreach (string s in Enum.GetNames(typeof(Knife.Activities))) { //access all possible knife activities, you should not need to use this
+			Debug.Log(s + " " + (int)Enum.Parse(typeof(Knife.Activities), s));
 		}
-		foreach (string s in Enum.GetNames(typeof(Kitchen.Kitchen_Generic_Activities))) {
-			Debug.Log(s + " " + (int)Enum.Parse(typeof(Kitchen.Kitchen_Generic_Activities), s))	;
+		foreach (string s in Enum.GetNames(typeof(Kitchen.Generic_Activities))) { //access all generic kitchen activities
+			Debug.Log(s + " " + (int)Enum.Parse(typeof(Kitchen.Generic_Activities), s))	;
 		}
-		Debug.Log(Kitchen.Kitchen_Generic_Activities.Cooking);
-		Debug.Log(Enum.GetName(typeof(Kitchen.Kitchen_Generic_Activities), 1));
+		Debug.Log(Kitchen.Generic_Activities.Cooking); //access a specific kitchen activity
+		Debug.Log(Enum.GetName(typeof(Kitchen.Generic_Activities), 1));
+		
+		System.Random rand = new System.Random();
+		Debug.Log((Kitchen.Generic_Activities) rand.Next(0,Kitchen.Num_Activities)); //how you may wish to randomize the activities
+		Debug.Log((Kitchen.Generic_Activities) rand.Next(0,Kitchen.Num_Activities));
+		Debug.Log((Kitchen.Generic_Activities) rand.Next(0,Kitchen.Num_Activities));
+		Debug.Log((Kitchen.Generic_Activities) rand.Next(0,Kitchen.Num_Activities));
+		
+		foreach (string s in Enum.GetNames(typeof(Living_Room.Generic_Activities))) {//access all generic living room activities
+			Debug.Log(s + " " + (int)Enum.Parse(typeof(Living_Room.Generic_Activities), s))	;
+		}
+		Debug.Log(Living_Room.screwdriver.activity); //access activity of screwdriver in living room
 	}
 	
 	// Update is called once per frame
--- trunk/Assets/Scripts/MurderData.cs
+++ trunk/Assets/Scripts/MurderData.cs
@@ -3,38 +3,200 @@ using System.Collections;
 
 namespace MurderData
 {	
-	public class Kitchen
+	/*
+	 * Rooms are defined here
+	 */ 
+	
+	public static class Kitchen
 	{
 		//define the activities not associated with weapons
-		public enum Kitchen_Generic_Activities
+		public enum Generic_Activities
 		{
-			Washing_Dishes,
-			Cooking,
-			Chatting_On_Phone,
-			Making_Tea
+			Washing_Dishes, 	//0
+			Cooking,			//1
+			Chatting_On_Phone,	//2
+			Making_Tea			//3
 		}
+		public static readonly int Num_Activities = 4;
 		
 		//define the weapons
-		public readonly Knife knife;
-		
-		public Kitchen() {
-			knife = new Knife(Knife.Knife_Activities.Cutting_Fruits);	//define the only possible activity in this room with this weapon
+		public static readonly Knife knife;
+		public static readonly Screwdriver screwdriver;
+		public static readonly Towel towel;
+		public static readonly Scissors scissors;
+		public static readonly Spanner spanner;
+		static Kitchen() {
+			knife = new Knife(Knife.Activities.Cutting_Fruits);	//define the only possible activity in this room with this weapon
+			screwdriver = new Screwdriver(Screwdriver.Activities.Fixing_Light);
+			towel = new Towel(Towel.Activities.Cleaning_Floor);
+			scissors = new Scissors(Scissors.Activities.Doing_Handicraft);
+			spanner = new Spanner(Spanner.Activities.Tightening_Tap);
 		}
 	}
+	
+	public static class Living_Room 
+	{
+		public enum Generic_Activities
+		{
+			Watching_TV,
+			Doing_Work,
+			Relaxing_On_Couch
+		}
+		public static readonly int Num_Activities = 3;
 		
+		public static readonly Knife knife;
+		public static readonly Screwdriver screwdriver;
+		public static readonly Towel towel;
+		public static readonly Scissors scissors;
+		public static readonly Spanner spanner;
+		static Living_Room() {
+			knife = new Knife(Knife.Activities.Cutting_Fruits);	//define the only possible activity in this room with this weapon
+			screwdriver = new Screwdriver(Screwdriver.Activities.Fixing_Light);
+			towel = new Towel(Towel.Activities.Cleaning_Floor);
+			scissors = new Scissors(Scissors.Activities.Doing_Handicraft);
+			spanner = new Spanner(Spanner.Activities.Tightening_Pipe);
+		}
+	}
+	
+	public static class Bedroom
+	{
+		public enum Generic_Activities
+		{
+			Sleeping,
+			Chatting_On_Phone,
+			Relaxing,
+			Reading_Book
+		}
+		public static readonly int Num_Activities = 4;
+		
+		public static readonly Knife knife;
+		public static readonly Screwdriver screwdriver;
+		public static readonly Towel towel;
+		public static readonly Scissors scissors;
+		public static readonly Spanner spanner;
+		static Bedroom() {
+			knife = new Knife(Knife.Activities.Cutting_Fruits);	//define the only possible activity in this room with this weapon
+			screwdriver = new Screwdriver(Screwdriver.Activities.Fixing_Light);
+			towel = new Towel(Towel.Activities.Cleaning_Floor);
+			scissors = new Scissors(Scissors.Activities.Doing_Handicraft);
+			spanner = new Spanner(Spanner.Activities.Tightening_Pipe);
+		}
+	}
+	
+	public static class Garden 
+	{
+		public enum Generic_Activities
+		{
+			Gardening,
+			Enjoying_The_View,
+			Watering_The_Plants
+		}
+		public static readonly int Num_Activities = 3;
+		
+		public static readonly Knife knife;
+		public static readonly Screwdriver screwdriver;
+		public static readonly Towel towel;
+		public static readonly Scissors scissors;
+		public static readonly Spanner spanner;
+		static Garden() {
+			knife = new Knife(Knife.Activities.Cutting_Fruits);	//define the only possible activity in this room with this weapon
+			screwdriver = new Screwdriver(Screwdriver.Activities.Fixing_Light);
+			towel = new Towel(Towel.Activities.Washing_Car);
+			scissors = new Scissors(Scissors.Activities.Pruning_Plants);
+			spanner = new Spanner(Spanner.Activities.Tightening_Pipe);
+		}
+	}
+	
+	public static class Toilet
+	{
+		public enum Generic_Activities
+		{
+			Cleaning,
+			Doing_Business
+		}
+		public static readonly int Num_Activities = 2;
+		
+		public static readonly Knife knife;
+		public static readonly Screwdriver screwdriver;
+		public static readonly Towel towel;
+		public static readonly Scissors scissors;
+		public static readonly Spanner spanner;
+		static Toilet() {
+			knife = new Knife(Knife.Activities.Cutting_Fruits);	//define the only possible activity in this room with this weapon
+			screwdriver = new Screwdriver(Screwdriver.Activities.Fixing_Light);
+			towel = new Towel(Towel.Activities.Bathing);
+			scissors = new Scissors(Scissors.Activities.Cutting_Hair);
+			spanner = new Spanner(Spanner.Activities.Tightening_Pipe);
+		}
+	}
+	
+	/*
+	 * Weapons are defined here 
+	 */
+	
 	public struct Knife 
 	{
 		//define the possible activities with the weapon
-		public enum Knife_Activities 
+		public enum Activities 
 		{
 			Cutting_Fruits,
 			Pruning_Plants
 		}
-		public readonly Knife_Activities ka;	//store the activity associated to the room
-		public Knife(Knife_Activities x) 
-		{
-			ka = x;	
-		}
+		public readonly Activities activity;	//store the activity associated to the room
+		public Knife(Activities x) { activity = x;	}
 	}
 	
+	public struct Screwdriver
+	{
+		public enum Activities { Fixing_Light }
+		public readonly Activities activity;
+		public Screwdriver (Activities x) { activity = x; }
+	}
+	
+	public struct Towel 
+	{
+		public enum Activities 
+		{
+			Cleaning_Floor,
+			Washing_Car,
+			Bathing
+		}
+		public readonly Activities activity;
+		public Towel(Activities x) { activity = x; }
+	}
+	
+	public struct Scissors 
+	{
+		public enum Activities 
+		{
+			Doing_Handicraft,
+			Pruning_Plants,
+			Cutting_Hair
+		}
+		public readonly Activities activity;
+		public Scissors(Activities x) { activity = x; }
+	}
+	
+	public struct Spanner
+	{
+		public enum Activities
+		{
+			Tightening_Tap,
+			Tightening_Pipe
+		}
+		public readonly Activities activity;
+		public Spanner(Activities x) { activity = x; }
+	}
+	
+	/*
+	 * Suspects are defined here
+	 */ 
+	
+	public enum Suspects
+	{
+		Wife,
+		Son,
+		Daughter,
+		Maid
+	}
 }
