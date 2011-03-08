77b0a37e729fa894c1b4fddc07f22dc5c0e52744
jxylow@gmail.com <jxylow@gmail.com@ab7b2411-665f-f69e-c7aa-cc4f6e78dff1>
2011-03-08 05:18:04 +0000

    Set the action key to be "E" instead of the left click. However, clicking on the text box causes Unity to crash. Still working on it. Weapons are set to highlight upon mouse hover.

--- trunk/Assets/Scripts/MouseOverScript.cs
+++ trunk/Assets/Scripts/MouseOverScript.cs
@@ -24,29 +24,36 @@ public class MouseOverScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		//AIlink = gameObject.GetComponent("AI") as AI;
-		
-		originalColor = new Color[targetObject.renderer.materials.Length];
-		for(int i = 0; i < targetObject.renderer.materials.Length; i++){
-			originalColor[i] = targetObject.renderer.materials[i].color;
+		if(targetObject.name == "knife1" || targetObject.name == "scissors" || targetObject.name == "spanner1" || targetObject.name == "screwdriver" || targetObject.name == "towel"){
+			originalColor = new Color[targetObject.renderer.materials.Length];
+			for(int i = 0; i < targetObject.renderer.materials.Length; i++){
+				originalColor[i] = targetObject.renderer.materials[i].color;
+			}
 		}
 	}
 	
 	void OnMouseEnter(){
 		//changing all the material to be yellow upon mouse hover, once the player is close to the object
-		if(withinBoundary){
-			//for(int i = 0; i < targetObject.renderer.materials.Length; i++){
-			//	targetObject.renderer.materials[i].color = mouseOverColor;
-			//}
+		if(targetObject.name == "knife1" || targetObject.name == "scissors" || targetObject.name == "spanner1" || targetObject.name == "screwdriver" || targetObject.name == "towel"){
+			//changing all the material to be yellow upon mouse hover, once the player is close to the object
+			if(withinBoundary){
+				for(int i = 0; i < targetObject.renderer.materials.Length; i++){
+					targetObject.renderer.materials[i].color = mouseOverColor;
+				}
+			}
 		}
 	}
 	
 	void OnMouseExit(){
 		//if user did not select the object
-		if(isSelected == false){
-			//changes back to the original material upon mouse exit
-			//for(int i = 0; i < targetObject.renderer.materials.Length; i++){
-			//	targetObject.renderer.materials[i].color = originalColor[i];
-			//}
+		if(targetObject.name == "knife1" || targetObject.name == "scissors" || targetObject.name == "spanner1" || targetObject.name == "screwdriver" || targetObject.name == "towel"){
+			//if user did not select the object
+			if(isSelected == false){
+				//changes back to the original material upon mouse exit
+				for(int i = 0; i < targetObject.renderer.materials.Length; i++){
+					targetObject.renderer.materials[i].color = originalColor[i];
+				}
+			}
 		}
 	}
 	
@@ -62,7 +69,7 @@ public class MouseOverScript : MonoBehaviour {
 		
 		//BUG is here
 		//action key is toggled every Left click , so it cannot click anything in the windows
-		if(isSelected == true && withinBoundary == true && actionKey == true){
+		if(withinBoundary == true && actionKey == true){
 			
 			
 	//if (withinBoundary && Input.GetMouseButtonDown(0))	{	
@@ -107,23 +114,23 @@ public class MouseOverScript : MonoBehaviour {
 	void Update () {
 		screenPos = Camera.main.WorldToScreenPoint(targetObject.position + offset);
 		
-		if(Input.GetMouseButtonDown(0)){
+		if(Input.GetKeyDown(KeyCode.E)){
 			
 			if(withinBoundary == true){
 				actionKey = !actionKey;
 				
-				if(actionKey){
-					isSelected = true;
-				}
-				else{
-					isSelected = false;
-				}
+				//~ if(actionKey){
+					//~ isSelected = true;
+				//~ }
+				//~ else{
+					//~ isSelected = false;
+				//~ }
 			}
 		}
 		
-		if(!(isSelected == true && withinBoundary == true)){
+		if(!(withinBoundary)){
 			actionKey = false;
-			isSelected = false;
+			//isSelected = false;
 		}
 	}
 }
