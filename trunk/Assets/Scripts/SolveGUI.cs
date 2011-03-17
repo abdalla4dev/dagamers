710648598a0ea9e592897beef95a8962a13b4623
menggnem <menggnem@ab7b2411-665f-f69e-c7aa-cc4f6e78dff1>
2011-03-17 07:32:27 +0000

    Timeline shows only when information is unlocked, GUI for solving the game is done

--- trunk/Assets/Scripts/SolveGUI.cs
+++ trunk/Assets/Scripts/SolveGUI.cs
@@ -10,14 +10,21 @@ public class SolveGUI : MonoBehaviour {
 	public GUIStyle buttonStyle;
 	
 	private bool showSolver = false;
-	private Rect windowRect = new Rect(20, 60, 600, 600);
-	private object selection;
-	private List<string> answers = new List<string>();
+	private Rect windowRect = new Rect(60, 60, 400, 400);
 	
-	public static object SelectList( ICollection list, object selected, GUIStyle defaultStyle, GUIStyle selectedStyle )
+	private string suspectSelection;
+	private List<string> suspectAnswers = new List<string>();
+	private string roomSelection;
+	private List<string> roomAnswers = new List<string>();
+	private string weaponSelection;
+	private List<string> weaponAnswers = new List<string>();
+	
+	private bool solved = false;
+	
+	public static string SelectList( IEnumerable<string> list, string selected, GUIStyle defaultStyle, GUIStyle selectedStyle )
 
         {         
-            foreach( object item in list )
+			foreach( string item in list )
             {
                 if( GUILayout.Button( item.ToString(), ( selected == item ) ? selectedStyle : defaultStyle ) )
                 {
@@ -32,23 +39,22 @@ public class SolveGUI : MonoBehaviour {
                     }
                 }
             }
-       
             return selected;
         }
        
        
        
-        public delegate bool OnListItemGUI( object item, bool selected, ICollection list );
+        public delegate bool OnListItemGUI( string item, bool selected, IEnumerable<string> list );
        
        
        
-        public static object SelectList( ICollection list, object selected, OnListItemGUI itemHandler )
+        public static string SelectList( IEnumerable<string> list, string selected, OnListItemGUI itemHandler )
         {
-            ArrayList itemList;
+            List<string> itemList;
            
-            itemList = new ArrayList( list );
+            itemList = new List<string>( list );
            
-            foreach( object item in itemList )
+            foreach( string item in itemList )
             {
                 if( itemHandler( item, item == selected, list ) )
                 {
@@ -64,7 +70,7 @@ public class SolveGUI : MonoBehaviour {
             return selected;
         }
 	
-	private bool OnCheckboxItemGUI( object item, bool selected, ICollection list )
+	private bool OnCheckboxItemGUI( string item, bool selected, IEnumerable<string> list )
     {
         return GUILayout.Toggle( selected, item.ToString() );
     }
@@ -72,7 +78,13 @@ public class SolveGUI : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		for (int i=0; i< Globals.numSuspects; i++) {
-			answers.Add(Enum.GetName(typeof(Suspects), i));
+			suspectAnswers.Add(Enum.GetName(typeof(Suspects), i));
+		}
+		for (int i=0; i<Globals.numRooms; i++) {
+			roomAnswers.Add(Enum.GetName(typeof(Rooms),i));
+		}
+		for (int i=0; i<Globals.numWeapons; i++) {
+			weaponAnswers.Add(Enum.GetName(typeof(Weapons),i));
 		}
 		//Debug.Log(answers[0] + " " +answers[1] + " " +answers[2] + " " +answers[3]);
 		/*answers.Add("Wife");
@@ -91,9 +103,28 @@ public class SolveGUI : MonoBehaviour {
 		GUILayout.Space(50);
 		if (GUILayout.Button(buttonTexture, buttonStyle, GUILayout.Height(25))) showSolver = !showSolver;
 		if (showSolver) GUILayout.Window(4, windowRect, SolveFunction, "Solve!");
+		
+		if (solved) GUI.Label(new Rect(Screen.width/2, Screen.height/2, 100,100), "You Won!");
 	}
 	
 	void SolveFunction(int windowID) {
-		selection = SelectList(answers ,selection, OnCheckboxItemGUI);
+		GUILayout.Label("The murderer was ...");
+		suspectSelection = SelectList(suspectAnswers ,suspectSelection, OnCheckboxItemGUI);
+		GUILayout.Label("using ...");
+		weaponSelection = SelectList(weaponAnswers ,weaponSelection, OnCheckboxItemGUI);
+		GUILayout.Label("in ...");
+		roomSelection = SelectList(roomAnswers ,roomSelection, OnCheckboxItemGUI);
+		if (GUILayout.Button("Solve!") ) {
+			List<string> answer = GenerateTimeline.murderTruth.getMurder();
+			if (answer[Person.place] == roomSelection && 
+				GenerateTimeline.murderWeap.ToString() == weaponSelection && 
+				Enum.GetName(typeof(Suspects), GenerateTimeline.murderer) == suspectSelection) {
+				//check if game solved
+				Debug.Log("Solved");
+				solved = true;
+			} else {
+				Debug.Log("Answer is " + answer[Person.place] + " " + GenerateTimeline.murderWeap.ToString() + " " +Enum.GetName(typeof(Suspects), GenerateTimeline.murderer));
+			}
+		}
 	}
 }
