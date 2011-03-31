7595e4cb0118d03a56a5c0af2c5bd6dadb2fb353
xariaa@gmail.com <xariaa@gmail.com@ab7b2411-665f-f69e-c7aa-cc4f6e78dff1>
2011-03-31 17:30:42 +0000

    added wpnfacts.

--- trunk/Assets/Scripts/Fact.cs
+++ trunk/Assets/Scripts/Fact.cs
@@ -8,6 +8,8 @@ public class Fact {
 	public readonly RmEnum placeRevealed;
 	public readonly TimelineSlot info;
 	public readonly SuspectEnum person;
+	public readonly WpnEnum weapon;
+	public readonly bool used;
 	
 	public Fact(RmEnum rm, TimelineSlot ts, SuspectEnum personn) {
 		placeRevealed = rm;
@@ -15,13 +17,47 @@ public class Fact {
 		person = personn;
 	}
 	
-	public string revealInfo() {
-		return "The CCTV recorded that " + person + 
-			" was doing " + info.activity + 
-				" in " + info.place + 
-				" with " + info.weapon + 
+	public Fact(TimelineSlot ts, SuspectEnum p, WpnEnum w, bool use)
+	{
+		info = ts;
+		person = p;
+		weapon = w;
+		used = use;
+	}
+	
+	public string revealInfo(string type) {
+		string toReturn = "";
+		if(type.Equals("CCTV"))
+		{
+			if(GenerateTimeline.difficulty!=GameDiffEnum.Hard)
+			{	toReturn = "The CCTV recorded that " + person + 
+					" was doing " + info.activity + 
+						" in " + info.place;
+				if(info.weapon!=WpnEnum.None)
+					toReturn += " with " + info.weapon;
+				toReturn += " from " + info.getStartTime() + 
+					" for " + info.getDuration() + 
+					".";
+			}
+			else
+				toReturn = "The CCTV recorded that " + person +
+				" was in " + info.place + 
 				" from " + info.getStartTime() + 
 				" for " + info.getDuration() + 
 				".";
+			
+		}
+		else if(type.Contains("weapon"))
+		{
+			
+			if(used)
+				toReturn = "Forensics has verified that someone has used the " + weapon.ToString() +
+					" recently to " +
+					info.activity + ". It could be the murder weapon.";
+			else
+				toReturn = "Forencis has verified that this item was not used recently.";
+		
+		}
+		return toReturn;
 	}
 }
