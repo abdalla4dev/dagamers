7b7f46b2c28a17162f9115145f5fdb2adcfdf4e4
wirelessed@gmail.com <wirelessed@gmail.com@ab7b2411-665f-f69e-c7aa-cc4f6e78dff1>
2011-04-04 15:39:13 +0000

    slight changes in loading screen

--- trunk/Assets/Scripts/loadingScreen.cs
+++ trunk/Assets/Scripts/loadingScreen.cs
@@ -5,20 +5,16 @@ public class loadingScreen : MonoBehaviour {
 	
 	private AsyncOperation async;
 
-	// Use this for initialization
 	void Start () {
 		async = Application.LoadLevelAsync ("main");
-    	//yield return async;
-		//Debug.Log("Loading complete");
 	}
 	
-	// Update is called once per frame
 	void Update () {
 
 	}
 	void OnGUI() {
-		if (async != null){
-			GUI.Box (new Rect(0, Screen.height - 40, async.progress * Screen.width, 40), "");	
-		}
+		//if (async != null){
+			GUI.Box(new Rect(0, Screen.height - 40, async.progress * Screen.width, 40), "");	
+		//}
 	}
 }
