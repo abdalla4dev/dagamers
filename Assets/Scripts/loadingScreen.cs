4e950929e22e9e189495dead4a2ab40ab1e7e97a
wirelessed <wirelessed@gmail.com>
2011-04-04 07:46:13 +0000

    loadingscreen script changed

--- Assets/Scripts/loadingScreen.cs
+++ Assets/Scripts/loadingScreen.cs
@@ -3,19 +3,22 @@ using System.Collections;
 
 public class loadingScreen : MonoBehaviour {
 	
-	AsyncOperation async;
-	float progress;
-	
+	private AsyncOperation async;
+
 	// Use this for initialization
-	IEnumerable Start () {
+	void Start () {
 		async = Application.LoadLevelAsync ("main");
-    	yield return async;
-		Debug.Log("Loading complete");
+    	//yield return async;
+		//Debug.Log("Loading complete");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 	}
-	
+	void OnGUI() {
+		if (async != null){
+			GUI.Box (new Rect(0, Screen.height - 40, async.progress * Screen.width, 40), "");	
+		}
+	}
 }
