b254c30f27d7c49c90f5fd4a34e4c09d1b260cb9
jxylow <jxylow@gmail.com>
2011-04-12 14:34:55 +0000

    COmpleted startmenu and difficulty levels.

--- Assets/Scripts/startMenuScript.cs
+++ Assets/Scripts/startMenuScript.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
 using System.Collections;
+using MurderData;
 
 public class startMenuScript : MonoBehaviour {
 	
 	public int buttonNumber;
 	public GameObject gameObject;
-
+	
+	public GameObject easyButton;
+	public GameObject mediumButton;
+	public GameObject hardButton;
+	
+	public static GameDiffEnum diffLevel;
+	bool diffToggle;
 	// Use this for initialization
 	void Start () {
-	
+		diffToggle = false;
 	}
 	
 	// Update is called once per frame
@@ -17,11 +24,21 @@ public class startMenuScript : MonoBehaviour {
 	}
 	
 	void OnMouseOver () {
-		iTween.RotateTo(gameObject,iTween.Hash("z", 388, "time", 0.5));
+		if(gameObject.name == "easyButton" || gameObject.name == "mediumButton" || gameObject.name == "hardButton"){
+			iTween.RotateTo(gameObject,iTween.Hash("z", 378, "time", 0.5));
+		}
+		else{
+			iTween.RotateTo(gameObject,iTween.Hash("z", 388, "time", 0.5));
+		}
 	}
 	
 	void OnMouseExit () {
-		iTween.RotateTo(gameObject,iTween.Hash("z", 348, "time", 0.5));
+		if(gameObject.name == "easyButton" || gameObject.name == "mediumButton" || gameObject.name == "hardButton"){
+			iTween.RotateTo(gameObject,iTween.Hash("z", 348, "time", 0.5));
+		}
+		else{
+			iTween.RotateTo(gameObject,iTween.Hash("z", 348, "time", 0.5));
+		}
 	}
 	
 	void OnMouseDown () {
@@ -29,7 +46,29 @@ public class startMenuScript : MonoBehaviour {
 			Application.LoadLevel("LoadingScreen"); 
 		}
 		else if (buttonNumber == 2){ // Difficulty
-			Application.LoadLevel("LoadingScreen"); 
+			diffToggle = !diffToggle;
+			if(diffToggle){
+				iTween.MoveBy(easyButton, iTween.Hash("x", -5.3, "time", 0.5));
+				iTween.MoveBy(mediumButton, iTween.Hash("x", -5.3, "time", 0.5));
+				iTween.MoveBy(hardButton, iTween.Hash("x", -5.3, "time", 0.5));
+			}
+			else{
+				iTween.MoveBy(easyButton, iTween.Hash("x", 5.3, "time", 0.5));
+				iTween.MoveBy(mediumButton, iTween.Hash("x", 5.3, "time", 0.5));
+				iTween.MoveBy(hardButton, iTween.Hash("x", 5.3, "time", 0.5));
+			}
+		}
+		else if(buttonNumber == 4){
+			diffLevel = MurderData.GameDiffEnum.Easy;
+			Application.LoadLevel("LoadingScreen");
+		}
+		else if(buttonNumber == 5){
+			diffLevel = MurderData.GameDiffEnum.Medium;
+			Application.LoadLevel("LoadingScreen");
+		}
+		else if(buttonNumber == 6){
+			diffLevel = MurderData.GameDiffEnum.Hard;
+			Application.LoadLevel("LoadingScreen");
 		}
 		else { // 3 = quit
 			var isWebPlayer = (Application.platform == RuntimePlatform.OSXWebPlayer || Application.platform == RuntimePlatform.WindowsWebPlayer);
