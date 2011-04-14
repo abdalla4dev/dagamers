9ed2fd5993d53946719baf6cec8298189266a7e2
wirelessed@gmail.com <wirelessed@gmail.com@ab7b2411-665f-f69e-c7aa-cc4f6e78dff1>
2011-04-14 16:53:32 +0000

    modified their lookAts.

--- trunk/Assets/Scripts/characterPaths.cs
+++ trunk/Assets/Scripts/characterPaths.cs
@@ -17,19 +17,20 @@ public class characterPaths : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+	
 		// resume walking if stopped
 		if(isStopped){
 			if(!withinBoundary){
 				iTween.Resume(gameObject,"MoveTo");
 				character.animation.CrossFade("walk");
 				isStopped = false;
-			}
+			}	
 		}
 		// stop when you approach him
 		if(withinBoundary){
 			// for animation walking
 			iTween.Pause(gameObject,"MoveTo");
-			iTween.LookTo(gameObject, iTween.Hash("looktarget", player.transform, "time", 1));
+			iTween.LookTo(gameObject, iTween.Hash("looktarget", player.transform, "axis", "y", "easetype", "easeInOutSine", "time", 1));
 			if (characterName == "theDaughter"){
 				character.animation.CrossFade("idle");
 			}
